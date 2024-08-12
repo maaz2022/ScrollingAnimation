44f8d60af7a23ab567b62ba76af1b89140c90281
maaz2022 <maaznazeer098@gmail.com>
2024-08-12 18:58:36 +0500

    camera position

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -4,7 +4,12 @@ import { Office } from "./Office";
 
 export const Experience = () => {
     return (
-        <Canvas>
+        <Canvas 
+        camera={{
+            fov: 64,
+            position: [2.3, 1.5, 2.3],
+        }}
+        >
             <ambientLight intensity={2}/>
             <OrbitControls enableZoom={false} />
             <ScrollControls pages={3} damping={0.25}>
